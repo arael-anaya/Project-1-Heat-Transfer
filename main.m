90886e508d2cf34b5071b7ba6fbb929bcfefb05e
Jordantehwizard <jordanrivera715278@gmail.com>
2026-02-06 00:05:04 -0700

    Hey dudeeee

--- main.m
+++ main.m
@@ -16,16 +16,56 @@ R_graphite=(log(r_3/r_2)/(2*pi*k_gr)); % [K*m/W] b/c cylinder length cancels
 
 T_2 = T_inf + q_1*(R_coolant + R_graphite); % [K]; temperature between thorium rod & graphite
 T_3 = T_inf + q_1*R_coolant; % [K]; temperature between graphite & coolant
-%T_1 is not shown b/c it'sthe temperature between the thorium rod & insulation
+
+r_th = linspace(r_1, r_2, 200);   % [m]
+
+% Preallocate temperature matrix
+T_th = zeros(length(q_dot_th), length(r_th));
+
+for i = 1:length(q_dot_th)
+
+    qth = q_dot_th(i);   % current volumetric heat generation [W/m^3]
+
+    % Integration constants
+    C1 = (qth/(2*k_th)) * r_1^2;
+    C2 = T_2(i) + (qth/(4*k_th))*r_2^2-(qth/(2*k_th))*r_1^2*log(r_2);
+
+    % Temperature distribution in thorium
+    T_th(i,:) = ...
+        -(qth/(4*k_th))*r_th.^2 ...
+        + C1*log(r_th) ...
+        + C2;
+end
+
+ 
+ 
+
 
 %Plotting
-r = [r_2*1000, r_3*1000]; %Convert into vector and m to mm for plotting purposes
-T = [T_2; T_3]; %Convert into vector for plotting purposes
+
+
 
 figure
-plot(r, T, 'LineWidth', 2)
+hold on
 grid on
+
+ax = gca;
+colors = ax.ColorOrder;   % MATLAB default color cycle
+
+% Thorium region (continuous)
+for i = 1:length(q_dot_th)
+    color = colors(mod(i-1, size(colors,1)) + 1, :);
+
+    plot(r_th*1000, T_th(i,:), ...
+        'LineWidth', 2, ...
+        'Color', color)
+
+    % Graphite + coolant nodes
+    plot([r_2, r_3]*1000, [T_2(i), T_3(i)],'LineWidth', 2, 'Color', color)
+end
 
 xlabel('Radius $r$ [mm]', 'Interpreter', 'latex')
 ylabel('Temperature $T$ [K]', 'Interpreter', 'latex')
-title('Temperature vs Radius', 'Interpreter', 'latex')
+title('Radial Temperature Distribution $T(r)$', 'Interpreter', 'latex')
+
+hold off
