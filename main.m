4918d8e9b8d0e6e6685503a79fd0ce638f65bb73
Jordantehwizard <jordanrivera715278@gmail.com>
2026-02-05 22:04:49 -0700

    Errrrm i think this is finished :)

--- main.m
+++ main.m
@@ -10,19 +10,22 @@ r_3=14/1000; %[m]
 q_dot_th=linspace(1e8, 5e8,5); %[W/m^3]
 
 %Calculations
-q_1=q_dot_th*pi*(r_2^2-r_1^2);
-R_coolant=1/(2*pi*r_3*h);
-R_graphite=(log(r_3/r_2)/(2*pi*k_gr));
+q_1=q_dot_th*pi*(r_2^2-r_1^2); %[W/m] b/c cylinder length is unavailable
+R_coolant=1/(2*pi*r_3*h); % [K*m/W] b/c cylinder length cancels
+R_graphite=(log(r_3/r_2)/(2*pi*k_gr)); % [K*m/W] b/c cylinder length cancels
 
-T_2 = T_inf + q_1*(R_coolant + R_graphite);
-T_3 = T_inf + q_1*R_coolant;
+T_2 = T_inf + q_1*(R_coolant + R_graphite); % [K]; temperature between thorium rod & graphite
+T_3 = T_inf + q_1*R_coolant; % [K]; temperature between graphite & coolant
+%T_1 is not shown b/c it'sthe temperature between the thorium rod & insulation
 
+%Plotting
+r = [r_2*1000, r_3*1000]; %Convert into vector and m to mm for plotting purposes
+T = [T_2; T_3]; %Convert into vector for plotting purposes
 
+figure
+plot(r, T, 'LineWidth', 2)
+grid on
 
-
-
-
-
-
-
-%xlabel('T_\infty')
+xlabel('Radius $r$ [mm]', 'Interpreter', 'latex')
+ylabel('Temperature $T$ [K]', 'Interpreter', 'latex')
+title('Temperature vs Radius', 'Interpreter', 'latex')
